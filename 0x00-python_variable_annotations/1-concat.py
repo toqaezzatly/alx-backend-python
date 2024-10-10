068a9ce1b4a212ec66d9b6c2fb83a3236c5b88dd
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-10-10 22:08:48 +0300

    check concatenation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+simple fuction returns 2 concatenated strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Retruns the concatenated strings"""
+    return str1 + str2
