a5cb46c35a3eeabc428f28a5c4f35276adea3fbc
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-10-11 00:42:51 +0300

    callable function

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -7,6 +7,7 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     def multiplier_function(value: float) -> float:
+        """value * multiplier"""
         return value * multiplier
     """callable function"""
     return multiplier_function
