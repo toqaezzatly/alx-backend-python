226cbf9ed89530bc046f4a3bb4fd0aa01952d8f1
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-10-10 22:42:22 +0300

    list into single sum

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import List, Union
 """
 type-anno take ints and floats -> float sum
 """
+from typing import List, Union
 
 
 def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
