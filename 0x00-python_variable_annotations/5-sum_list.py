bfa365f77b41e20f131f39ea0f130fceed01810d
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-10-10 22:33:32 +0300

    list into single sum

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+type-annotated function takes lst of floats -> float sum
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """list of float to float sum"""
+    return sum(input_list)
