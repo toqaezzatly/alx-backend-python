8205ab5d2963a20a9efaf11222a03b40b7752adb
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-10-10 22:12:52 +0300

    floored

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import math
+"""
+Takes float and truns it into integer
+"""
+
+
+def floor(n: float) -> int:
+    """Return the floor of the integer"""
+    return math.floor(n)
