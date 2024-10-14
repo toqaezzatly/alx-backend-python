19349165acb2f8f746d01ecbbed47312abadb514
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-10-14 19:13:04 +0300

    UNPACKIN TO GATHER

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+file for printing the correct output of the wait_n coroutine
+'''
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    Executes wait_random n times
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+        )
+    return sorted(wait_times)
