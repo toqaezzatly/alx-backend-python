b8f599abe449f3df70db2240b2e0a683b4d37c51
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-11-04 18:20:29 +0200

    if u don't raise error u r not my boy

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,6 +16,17 @@ class TestAccessNestedMap(unittest.TestCase):
         """the function to be tested """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """test errors"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception), repr(path[-1]))
+
 
 if __name__ == "__main__":
     unittest.main()
